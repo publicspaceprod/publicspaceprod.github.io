68b170d6e7f426f7e78ff88c208f9c3e12d15e3b
Miguel Ocampo-Gooding <migueog@gmail.com>
2018-06-18 01:57:55 -0400

    Window onload to document ready

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var preloader = document.getElementById('preloader');
     var logoanim = document.getElementById('splash__logo');
     // preloader
-    $(window).on('load', function(){
+    $(document).ready(function(){
         if (window.location.href === 'http://127.0.0.1:4000/' || window.location.href === 'https://publicspaceprod.github.io/' || window.location.href === 'https://publicspaceproductions.com/') {
             $(preloader).css('display', 'none');
             document.getElementById('splash__logo').addEventListener('ended',videoEnd,false);
